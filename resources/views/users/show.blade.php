fc5b45a9b87c11ad4deb6f4992e97df56c79af3f
j.ishii <nohjxv@gmail.com>
2021-03-06 06:13:01 +0000

    timeline

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
+           <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">{{ $user->name }}</h3>
                 </div>
@@ -12,16 +12,18 @@
                     <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
                 </div>
             </div>
+            {{-- フォロー／アンフォローボタン --}}
+            @include('user_follow.follow_button')
         </aside>
         <div class="col-sm-8">
-            <ul class="nav nav-tabs nav-justified mb-3">
-                {{-- ユーザ詳細タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
-                {{-- フォロー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
-                {{-- フォロワー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>
+            {{-- タブ --}}
+            @include('users.navtabs')
+            @if (Auth::id() == $user->id)
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+            @endif
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
         </div>
     </div>
 @endsection
